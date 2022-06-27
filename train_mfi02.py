57f6a57c19bffebe069f6f2d320899c39590ae30
mfi_machine <mathias.fiedler86@gmail.com>
2022-06-27 20:04:26 +0200

    added a random student class

--- train_mfi02.py
+++ train_mfi02.py
@@ -1,1 +1,3 @@
 print("Hier kommt ein erster Text")
+
+print("Hier für den newbranch")
--- train_mfi03.py
+++ train_mfi03.py
@@ -1,3 +1,23 @@
+import random
+
+
 for x in range(10):
     y = x * x
     print(y)
+
+
+class Student:
+    counter = 0
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Student.counter += 1
+
+
+s1 = Student("Maik", 20)
+s2 = Student("Kira", 19)
+li_students = ["Kay", "Horst", "Steph"]
+for name in li_students:
+    st = Student(name, random.randint(19, 25))
+print(f"Number of Students: {Student.counter}")
